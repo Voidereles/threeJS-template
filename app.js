b0e6230ded1c18228cb1ac8cdd4faa9b4fda832a
Maciej <maciej.zelazo@protonmail.com>
2020-11-14 21:59:33 +0100

    OrbitControls

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import * as THREE from './node_modules/three/build/three.module.js';
 import fragment from './shaders/fragment.glsl';
 import vertex from './shaders/vertex.glsl';
 
+let OrbitControls = require("three-orbit-controls")(THREE);
+
 export default class Sketch {
     constructor() {
 
@@ -15,25 +17,14 @@ export default class Sketch {
         this.camera.position.z = 1000;
 
         this.scene = new THREE.Scene();
-
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
 
         this.addMesh();
-
-        this.time = 0;
         this.render();
     }
 
     addMesh() {
-        this.geometry = new THREE.PlaneBufferGeometry(1000, 1000, 10, 10);
-        this.geometry = new THREE.BufferGeometry();
-        let number = 512 * 512;
-
-        this.positions = new THREE.BufferAttribute(new Float32Array(number * 3));
-
-        this.material = new THREE.MeshNormalMaterial({
-            side: THREE.DoubleSide
-        });
         this.material = new THREE.ShaderMaterial({
             fragmentShader: fragment,
             vertexShader: vertex,
@@ -46,13 +37,33 @@ export default class Sketch {
             side: THREE.DoubleSide
         });
 
+        let number = 512 * 512;
+        // this.geometry = new THREE.PlaneBufferGeometry(1000, 1000, 10, 10);
+        this.geometry = new THREE.BufferGeometry();
+
+        this.positions = new THREE.BufferAttribute(new Float32Array(number * 3), 3);
+
+        // this.material = new THREE.MeshNormalMaterial({
+        //     side: THREE.DoubleSide
+        // });
+        let index = 0;
+        for (let i = 0; i < 512; i++) {
+            let posX = i - 256;
+            for (let j = 0; j < 512; j++) {
+                this.positions.setXYZ(index * 3, i * 2, (j - 256) * 2, 0);
+                index++;
+            }
+        }
+
+        this.geometry.setAttribute("position", this.positions);
+
         this.mesh = new THREE.Points(this.geometry, this.material);
         this.scene.add(this.mesh);
     }
 
     render() {
         this.time++;
-
+        //rotation
         this.mesh.rotation.x += 0.01;
         this.mesh.rotation.y += 0.02;
 
